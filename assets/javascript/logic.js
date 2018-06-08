0648bc7a23271369afbc9fd2f7b31b6b3a269954
wood0761 <wood0761@gmail.com>
2018-06-08 10:50:35 -0500

    media queries

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
 
     setInterval(function(){
          $(".jumbotron").toggle()}, 3000);
-    }); 
+
+    if ($(window).width() >= 760) {
+        $(".wrappingContainer").addClass("container");
+    } 
+
+
+}); 
