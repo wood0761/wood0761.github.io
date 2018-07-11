e8078e1ef70e0fdb50fde3208e28649352bad709
wood0761 <wood0761@gmail.com>
2018-07-11 16:03:19 -0500

    new commit

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     setInterval(function(){
          $(".jumbotron").toggle()}, 3000);
 
-    if ($(window).width() >= 760) {
+    if ($(window).width() >= 760) {                     // on load (not responsive), if width is ipad or wider, entire body gets a container
         $(".wrappingContainer").addClass("container");
     } 
 
